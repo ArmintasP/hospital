4fa69237e37493517673b2fd5645988e379d1421
ArmintasP <75906786+ArmintasP@users.noreply.github.com>
2023-05-08 18:04:50 +0300

    Add first sample xhtml.

--- src/main/java/com/example/hospital/usecases/myBatis/DepartmentsMyBatis.java
+++ src/main/java/com/example/hospital/usecases/myBatis/DepartmentsMyBatis.java
@@ -1,42 +1,41 @@
-//package com.example.hospital.usecases.myBatis;
-//
-//import com.example.hospital.mybatis.dao.DepartmentMapper;
-//import com.example.hospital.mybatis.model.Department;
-//import com.example.hospital.mybatis.model.DepartmentExample;
-//import jakarta.annotation.PostConstruct;
-//import jakarta.enterprise.inject.Model;
-//import jakarta.inject.Inject;
-//import jakarta.transaction.Transactional;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import java.util.List;
-//
-//@Model
-//public class DepartmentsMyBatis {
-//    @Inject
-//    private DepartmentMapper departmentMapper;
-//
-//    @Getter
-//    private List<Department> departments;
-//
-//    @Getter
-//    @Setter
-//    private Department newDepartment = new Department();
-//
-//    @PostConstruct
-//    public void init() {
-//        getAllDepartments();
-//    }
-//
-//    private void getAllDepartments() {
-//        departments = departmentMapper.selectByExample(new DepartmentExample());
-//    }
-//
-//    @Transactional
-//    public String create() {
-//        departmentMapper.insert(newDepartment);
-//        return "/myBatis/departments?faces-redirect=true";
-//    }
-//}
-//
+package com.example.hospital.usecases.myBatis;
+
+import com.example.hospital.mybatis.dao.DepartmentMapper;
+import com.example.hospital.mybatis.model.Department;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.inject.Model;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Model
+public class DepartmentsMyBatis {
+    @Inject
+    private DepartmentMapper departmentMapper;
+
+    @Getter
+    private List<Department> departments;
+
+    @Getter
+    @Setter
+    private Department newDepartment = new Department();
+
+    @PostConstruct
+    public void init() {
+        getAllDepartments();
+    }
+
+    private void getAllDepartments() {
+        departments = departmentMapper.selectAll();
+    }
+
+    @Transactional
+    public String create() {
+        departmentMapper.insert(newDepartment);
+        return "/myBatis/departments?faces-redirect=true";
+    }
+}
+
